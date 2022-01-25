e6a5da3b176d37783587d62efd47eee63d323df9
Amal John <73713382+amaljohn2304@users.noreply.github.com>
2022-01-25 10:49:25 +0530

    Add files via upload

---
+++ frontend/src/screens/MapScreen/MapScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Bar from "../../components/SearchBar/Bar"
+
+function MapScreen() {
+    return <div>
+        <Bar />
+    </div>;
+}
+
+export default MapScreen;
